13b73bedbb65d7d4d541bdf89d5ea56bd24e931b
Emil Martinov <martinov.emil@gmail.com>
2020-08-03 17:32:04 +0200

    Start screen implemented

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,52 @@
 <script>
+	import {onMount} from 'svelte'
 	import Welcome from "./screens/Welcome.svelte"
-	let state = "welcome";
+	import {select} from "./select.js"
+	
+	let state = "welcome"; 
+	let selection
+	let celebsPromise;
+	const start = async (e) => {
+		const {celebs, lookup} = await celebsPromise;
+
+		selection = select(celebs, lookup, e.detail.category.slug)
+		state = "playing"
+	}
+	const loadCelebs = async () => {
+		const res = await fetch("https://cameo-explorer.netlify.app/celebs.json")
+		const data = await res.json();
+
+		const lookup = new Map()
+
+		data.forEach(c=> {
+			lookup.set(c.id, c)
+		})
+
+		const subset = new Set()
+		data.forEach(c=> {
+			if (c.reviews >= 50) {
+				subset.add(c);
+				c.similar.forEach(id=>{
+					subset.add(lookup.get(id))
+				})
+			}
+		})
+
+		return {
+			celebs: Array.from(subset),
+			lookup
+		}
+
+		console.log(data);
+	}
+	onMount(()=>{
+		celebsPromise = loadCelebs()
+	})
 </script>
 
 <main>
 	{#if state=== "welcome"}
-		<Welcome/>
+		<Welcome on:select={start}/>
 	{:else if state==="playing"}
 		<p>Game screen goes here</p>
 	{/if}
--- src/screens/Welcome.svelte
+++ src/screens/Welcome.svelte
@@ -1,56 +1,88 @@
-    <header><h1 class="svelte-1fhxxll">
-		CameoP<span class="logo svelte-1fhxxll">a</span>rison
-	</h1> 
+<script>
+    import {createEventDispatcher} from 'svelte';
 
-	<p class="svelte-1fhxxll">
-		On <a href="https://cameo.com">cameo.com</a>, you can buy personalised video clips from everyone from Lindsay Lohan to Ice T.
-	</p> 
+    const dispatch = createEventDispatcher();
+    let selected = false;
+    const select = category => {
+        selected = true;
+        console.log('selected', category)
+        dispatch('select', {category})
+    }
 
-    <p class="svelte-1fhxxll">But who commands the highest price?</p></header>
-    
-    <style>
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: min(12vw, 4em);
-		font-weight: 100;
-		margin: 0 0 0.5em 0;
-	}
-    p {
-		max-width: 24em;
-		margin: 0 auto 1em auto;
-	}
+    const categories= [
+    { slug: 'actors', label: 'Actors'},
+    { slug: 'athletes', label: 'Athletes'},
+    { slug: 'comedians', label: 'Comedians'},
+    { slug: 'creators', label: 'Creators'},
+    { slug: 'models', label: 'Models'},
+    { slug: 'musicians', label: 'Musicians'},
+    { slug: 'reality-tv', label: 'Reality TV'},
+    { slug: 'comedians', label: 'Comedians'},
+]
+</script>
 
-	.logo {
-		display: inline-block;
-		background: url(/icons/compare.svg) 50% 50% no-repeat;
-		background-size: 100% 100%;
-		width: 0.8em;
-		top: 0.05em;
-		transform: scale(1.4);
-		left: 0.02em;
-		text-indent: -9999px;
-	}
+<header><h1 class="svelte-1fhxxll">
+    CameoP<span class="logo svelte-1fhxxll">a</span>rison
+</h1> 
 
-	.categories {
-		width: 100%;
-		max-width: 26em;
-		margin: 0 auto;
-	}
+<p class="svelte-1fhxxll">
+    On <a href="https://cameo.com">cameo.com</a>, you can buy personalised video clips from everyone from Lindsay Lohan to Ice T.
+</p> 
 
-	button {
-		padding: 1em;
-		display: block;
-		margin: 0 0 0.2em 0;
-		width: 100%;
-	}
+<p class="svelte-1fhxxll">But who commands the highest price?</p></header>
 
-	@media (min-width: 640px) {
-		button {
-			display: inline;
-			margin: 0 0.2em 0.4em 0.2em;
-			width: auto;
-		}
-	}
+<p>Pick a category to play a game:</p>
 
-    </style>
+<div class="categories">
+    {#each categories as category}
+        <button on:click={()=> select(category)} disabled={selected}>
+            {category.label}
+        </button>
+    {/each}
+</div>
+<style>
+h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: min(12vw, 4em);
+    font-weight: 100;
+    margin: 0 0 0.5em 0;
+}
+p {
+    max-width: 24em;
+    margin: 0 auto 1em auto;
+}
+
+.logo {
+    display: inline-block;
+    background: url(/icons/compare.svg) 50% 50% no-repeat;
+    background-size: 100% 100%;
+    width: 0.8em;
+    top: 0.05em;
+    transform: scale(1.4);
+    left: 0.02em;
+    text-indent: -9999px;
+}
+
+.categories {
+    width: 100%;
+    max-width: 26em;
+    margin: 0 auto;
+}
+
+button {
+    padding: 1em;
+    display: block;
+    margin: 0 0 0.2em 0;
+    width: 100%;
+}
+
+@media (min-width: 640px) {
+    button {
+        display: inline;
+        margin: 0 0.2em 0.4em 0.2em;
+        width: auto;
+    }
+}
+
+</style>
